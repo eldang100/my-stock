1a2c95bf064ee99da756088af44e537b1b42617e
eldang100 <eldang100@naver.com>
2026-06-23 18:17:35 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 
 self.addEventListener('fetch', e => {
+  // 같은 출처 GET 요청만 캐시 처리
   if (e.request.method !== 'GET') return;
   e.respondWith(
     caches.open(CACHE).then(c =>
